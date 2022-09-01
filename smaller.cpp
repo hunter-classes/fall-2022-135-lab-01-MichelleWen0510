fe15d085cc9fad96355347c124010e1cd5ee7b79
Michelle <Michelle@Johns-MacBook-Pro-2.local>
2022-09-01 11:01:57 -0400

    added smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Michelle Wen
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 1 Task A
+
+Find the smaller number of two inputs.
+*/
+#include <iostream>
+int main()
+{
+	int x;
+	int y;
+	std::cout << "Enter the first number: ";
+	std::cin >> x;
+	std::cout << "Enter the second number: ";
+	std::cin >> y;
+	if (x < y)
+	{
+		std::cout << "The smaller of the two is "+ std::to_string(x);
+	} else {
+		std::cout << "The smaller of the two is "+ std::to_string(y);
+	}
+	return 0;
+}
+
